3e1bf7e99c4b398d5a72dad7ab24c1ee2bac9fd0
Camila <camila.t.0029@gmail.com>
2026-05-08 11:53:22 -0300

    Aula JavaScript

---
+++ JavaScript/variaveis.js
@@ -0,0 +1,26 @@
+let a = 10;
+console.log(a);
+console.log(typeof(a));
+
+a = "dez";
+console.log(a);
+console.log(typeof(a));
+
+a = true
+console.log(a)
+console.log(typeof(a))
+
+a = 5
+console.log(a + "0")
+console.log("0" + 5)
+
+const pi = 3.14156 //O que é PI
+// pi=2 -> Não pode
+
+let contador = 0
+contador++
+console.log(contador)
+
+for(contador=0; contador<10; contador++){
+    console.log(contador)
+}
